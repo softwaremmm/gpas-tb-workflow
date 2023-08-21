2a305a8bc9714904f8c83f694e022cdca1a0a31f
JeremyWesthead <jeremywesthead3@gmail.com>
2023-08-21 16:49:08 +0100

    Integrate into main release action

--- jsonify-software-versions.py
+++ jsonify-software-versions.py
@@ -20,9 +20,6 @@ if __name__ == "__main__":
     # <container name> --> <command to get software version> --> <software version>
     data = [line.split(" --> ") for line in data]
 
-    for line in data:
-        print(line)
-
     # This should be written as a JSON of:
     # {
     #   <container>: {
