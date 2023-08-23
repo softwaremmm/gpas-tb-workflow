22b3dc320aaa7e5efd1046a9316152ac80ba68ba
JeremyWesthead <jeremywesthead3@gmail.com>
2023-08-23 10:59:43 +0100

    Added helper script to ensure test species

---
+++ run_with_test_species.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -profile)
+      PROFILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --sample_id)
+      SAMPLE_ID="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --run_id)
+      RUN_ID="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --api_token)
+      API_TOKEN="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+echo "PROFILE   = ${PROFILE}"
+echo "SAMPLE ID = ${SAMPLE_ID}"
+echo "RUN ID    = ${RUN_ID}"
+echo "API TOKEN = ${API_TOKEN}"
+
+nextflow run . -profile $PROFILE --sample_id $SAMPLE_ID --run_id $RUN_ID --api_token $API_TOKEN --species test
+
