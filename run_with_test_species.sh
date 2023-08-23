4894e4ac7060b70d9bc83c40ff8c5212710359a7
JeremyWesthead <jeremywesthead3@gmail.com>
2023-08-23 11:45:47 +0100

    Add api token guard

--- run_with_test_species.sh
+++ run_with_test_species.sh
@@ -36,7 +36,13 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 echo "PROFILE   = ${PROFILE}"
 echo "SAMPLE ID = ${SAMPLE_ID}"
 echo "RUN ID    = ${RUN_ID}"
-echo "API TOKEN = ${API_TOKEN}"
+if [ -z "$API_TOKEN" ]
+then
+    echo "Missing --api_token!"
+    exit 1
+else
+    echo "API_TOKEN set"
+fi
 
-nextflow run . -profile $PROFILE --sample_id $SAMPLE_ID --run_id $RUN_ID --api_token $API_TOKEN --species test
+echo nextflow run . -profile $PROFILE --sample_id $SAMPLE_ID --run_id $RUN_ID --api_token $API_TOKEN --species test
 
