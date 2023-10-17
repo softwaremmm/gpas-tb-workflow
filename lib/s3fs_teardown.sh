7ce9f61acfc22db25180a2c8f92aae89d3a15502
Michael Park <me@mhpark.me>
2023-10-17 09:06:53 +0100

    fix(umount): try to umount but don't fail if already umounted
    Sub processes are calling umount so the parent was trying to call
    umount but not succeeding. This should allow for you to try to
    umount and assume that you have already if it fails.

--- lib/s3fs_teardown.sh
+++ lib/s3fs_teardown.sh
@@ -1,2 +1,3 @@
-umount /workspace/buckets/upload_bucket; umount /workspace/buckets/input_bucket; umount /workspace/buckets/output_bucket; umount /workspace/buckets/relatedness_bucket
-echo "Teardown successful"
+#!/usr/bin/env bash
+umount /workspace/buckets/upload_bucket; umount /workspace/buckets/input_bucket; umount /workspace/buckets/output_bucket; umount /workspace/buckets/relatedness_bucket; true
+echo 'Umount completed'
