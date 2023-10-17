aecf4eef97f83af99f2eb3e04000c672a67ce8c0
bobturneruk <bob.turner.uk@gmail.com>
2023-10-17 12:24:14 +0100

    fix: Remove echo on unmount completed

--- lib/s3fs_teardown.sh
+++ lib/s3fs_teardown.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 umount /workspace/buckets/upload_bucket; umount /workspace/buckets/input_bucket; umount /workspace/buckets/output_bucket; umount /workspace/buckets/relatedness_bucket; true
-echo 'Umount completed'
