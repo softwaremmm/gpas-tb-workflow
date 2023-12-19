af10d76637c7d8fa24947fa04c3dad4b8d4c6767
JeremyWesthead <jeremywesthead3@gmail.com>
2023-12-19 11:32:59 +0000

    feat: use hostpath mounted buckets

--- lib/s3fs_teardown.sh
+++ lib/s3fs_teardown.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-#Intentionally not using `set -e` so it tries everything
-
-#Make sure the buckets are unmounted
-umount /workspace/buckets/upload_bucket
-umount /workspace/buckets/input_bucket
-umount /workspace/buckets/output_bucket
-umount /workspace/buckets/relatedness_bucket
-
-# Clear up the password file now it's not needed
-rm s3fs_password_file
+echo "Would have unmounted buckets here"
