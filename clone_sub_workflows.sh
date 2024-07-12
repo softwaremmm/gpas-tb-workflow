f7796c80c4927892860bc1c590b48dfd66618650
Matthew Colpus <mpcolpus@gmail.com>
2024-07-12 10:20:21 +0000

    fix: set desired branches for subworkflows

--- clone_sub_workflows.sh
+++ clone_sub_workflows.sh
@@ -6,19 +6,20 @@ LATEST=$1
 
 cd sub_workflows
 
-for p in $(cat ../includerepos.txt); do
-  echo $p 
-  git clone git@github.com:"$p" --depth 1 -b main
+while IFS=, read -r repo branch
+do
+  echo $repo 
+  git clone git@github.com:"$repo" --depth 1 -b $branch
 
   if [ -n "${LATEST}" ]; then
     #If requested, use the latest release rather than main
-    cd $(basename $p)
+    cd $(basename $repo)
     git config advice.detachedHead false
     pipeline_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
     git checkout $pipeline_tag
     cd ..
   fi
   echo 
-done
+done < "../includerepos.csv"
 
 cd ..
