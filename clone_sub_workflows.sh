2259e2e91bd4da78c5f565b29c53adf44a9092d6
bobturneruk <bob.turner.uk@gmail.com>
2026-06-08 15:50:02 +0000

    fix: use ssh for local git clone

--- clone_sub_workflows.sh
+++ clone_sub_workflows.sh
@@ -11,7 +11,7 @@ jq -r 'to_entries[] | "\(.key) \(.value)"' ../pipeline_versions.json |
 while read repo identififer; do
   echo $repo $identififer
   rm -rf $repo
-  git clone https://github.com/GlobalPathogenAnalysisService/"$repo"
+  git clone git@github.com:"$repo"
   cd $repo
   git config advice.detachedHead false
   git checkout $identififer
