b8d7209b6e8e7977236a889946fe9c6afed00b5c
bobturneruk <bob.turner.uk@gmail.com>
2026-07-01 12:11:10 +0000

    chore: makefile to facilitate local running

--- setup_local_example.sh
+++ setup_local_example.sh
@@ -1,9 +1,8 @@
-mkdir -p data/inputs/1/1
-mkdir -p data/inputs/2/1
-mkdir -p data/outputs
+mkdir -p data/inputs/illumina
+mkdir -p data/inputs/ont
 
 # download simple examples
-wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR479/004/ERR4796484/ERR4796484_1.fastq.gz -O data/inputs/1/1/ERR4796484_1.fastq.gz
-wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR479/004/ERR4796484/ERR4796484_2.fastq.gz -O data/inputs/1/1/ERR4796484_2.fastq.gz
+wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR479/004/ERR4796484/ERR4796484_1.fastq.gz -O data/inputs/illumina/ERR4796484_1.fastq.gz
+wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR479/004/ERR4796484/ERR4796484_2.fastq.gz -O data/inputs/illumina/ERR4796484_2.fastq.gz
 
-wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR903/006/ERR9030326/ERR9030326.fastq.gz -O data/inputs/2/1/ERR9030326.fastq.gz
+wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR903/006/ERR9030326/ERR9030326.fastq.gz -O data/inputs/ont/ERR9030326.fastq.gz
