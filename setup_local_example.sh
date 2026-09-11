c6bd3e600987567df5080b1d4b60ee58da4a4fe0
Matthew Colpus <mpcolpus@gmail.com>
2026-09-11 12:00:48 +0000

    fix: only download abscessus when requested

--- setup_local_example.sh
+++ setup_local_example.sh
@@ -7,19 +7,19 @@ wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR479/004/ERR4796484/ERR4796484_2.fastq.gz -O
 
 wget ftp.sra.ebi.ac.uk/vol1/fastq/ERR903/006/ERR9030326/ERR9030326.fastq.gz -O data/inputs/ont/ERR9030326.fastq.gz
 
-# download abscessus
-mkdir -p data/inputs/abscessus/illumina
-mkdir -p data/inputs/abscessus/ont
 
-wget ftp.sra.ebi.ac.uk/vol1/fastq/SRR320/050/SRR32024550/SRR32024550_1.fastq.gz -O data/inputs/abscessus/illumina/SRR32024550_1.fastq.gz
-wget ftp.sra.ebi.ac.uk/vol1/fastq/SRR320/050/SRR32024550/SRR32024550_2.fastq.gz -O data/inputs/abscessus/illumina/SRR32024550_2.fastq.gz
-
-wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR160/065/ERR16089665/ERR16089665.fastq.gz -O data/inputs/abscessus/ont/ERR16089665.fastq.gz
-
-# use seqkit to make a 50 50 mix
 MAKE_MIX=false
-
 if [ "$MAKE_MIX" = true ]; then
+    # download abscessus
+    mkdir -p data/inputs/abscessus/illumina
+    mkdir -p data/inputs/abscessus/ont
+
+    wget ftp.sra.ebi.ac.uk/vol1/fastq/SRR320/050/SRR32024550/SRR32024550_1.fastq.gz -O data/inputs/abscessus/illumina/SRR32024550_1.fastq.gz
+    wget ftp.sra.ebi.ac.uk/vol1/fastq/SRR320/050/SRR32024550/SRR32024550_2.fastq.gz -O data/inputs/abscessus/illumina/SRR32024550_2.fastq.gz
+
+    wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR160/065/ERR16089665/ERR16089665.fastq.gz -O data/inputs/abscessus/ont/ERR16089665.fastq.gz
+
+    # use seqkit to make a 50 50 mix
     mkdir -p data/inputs/mix/illumina
     mkdir -p data/inputs/mix/ont
     numreads=100000
